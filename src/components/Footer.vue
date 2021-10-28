a7d906cfc763f9b37b26121130c8dc7c872bf449
lucas mira <lucas-mira1@hotmail.com>
2021-10-28 20:28:38 -0300

    add navbar and footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="page-footer">
+    <p>
+      &copy; <span id="date">2021</span>
+      <span class="footer-logo"> SimplyRecipes</span>
+      Built by <a href="https://www.johnsmilga.com/">Coding Addict</a>
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
